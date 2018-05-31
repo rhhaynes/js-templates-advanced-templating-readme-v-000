20fc71c32c275df358b61e7f32c4991b3de7aa0a
Randall Harris Haynes <harrishaynes@gmail.com>
2018-05-31 19:29:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-function loadIssue() {
+function loadIssues() {
   const template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
-  for (let i=0;i<issues.length;i++) {
-    const result = template(issues[i]);
-    document.getElementsByTagName("main")[0].innerHTML += result;
-  }
+  const result = template(issues);
+  document.getElementsByTagName("main")[0].innerHTML += result;
 }
